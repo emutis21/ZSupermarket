c2cf190e647c110076ab8cf278fe2a5232a46f14
Esteban Mutis <smithmutis21@gmail.com>
2024-02-19 11:10:20 -0500

    Refactor ReturnButton component to use Next.js Link

--- src/components/ReturnButton.tsx
+++ src/components/ReturnButton.tsx
@@ -1,25 +1,21 @@
+'use client'
+
+import Link from 'next/link'
 import { ReturnIcon } from './icons/ReturnIcon'
 
-export const ReturnHome = () => {
-  const goBack = () => {
-    if (typeof window !== 'undefined') {
-      if (document.referrer === '' || window.history.length <= 2) {
-        window.location.href = '/products'
-      } else {
-        window.history.back()
-      }
-    }
-  }
+export const ReturnButton = ({ id }: { id?: number }) => {
+  const goBack = id ? `/products/${id}` : '/products'
 
   return (
-    <button
-      onClick={goBack}
+    <Link
+      prefetch={false}
+      href={goBack}
       data-ripple-light='true'
       type='button'
       className='select-none flex items-center w-fit underline hover:text-white rounded-lg text-blue-500 py-3 absolute gap-2 text-center align-middle font-sans text-xs font-bold uppercase shadow-md transition-all focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
     >
       <ReturnIcon />
       Volver
-    </button>
+    </Link>
   )
 }
